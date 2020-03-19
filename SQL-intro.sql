1bdbf9246bb79a45750c68d6d89d83fb7dd5adeb
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-03-19 10:29:49 -0500

    used ORDER BY and LEFT JOIN to order the data by song count on different instances

--- SQL-intro.sql
+++ SQL-intro.sql
@@ -29,3 +29,39 @@ SELECT * FROM Album
 INSERT INTO Song (Title, ReleaseDate, ArtistId, SongLength, GenreId, AlbumId) VALUES ('Test Song 2', '3/18/2020', 28, 61,  1, 23)
 
 SELECT * FROM Song
+
+	--SELECT a.Title, s.Title, ar.ArtistName, g.Label
+	--FROM Song s
+	--LEFT JOIN Album a
+	--on s.AlbumId = a.Id
+	--LEFT JOIN Artist ar
+	--ON s.ArtistId = ar.Id
+	--LEFT JOIN Genre g
+	--ON s.GenreId = g.Id
+
+--ordering he albums by amount of songs and displaying the number of songs per album
+SELECT COUNT(AlbumId)
+AS 'Song Count', a.Title
+FROM Song s
+LEFT JOIN Album a
+ON s.AlbumId = a.Id
+GROUP BY AlbumId, a.Title
+ORDER BY COUNT(AlbumId);
+
+--ordering he artist by amount of songs and displaying the number of songs per artist
+SELECT COUNT(ArtistId)
+AS 'Song Count', ar.ArtistName
+FROM Song s
+LEFT JOIN Artist ar
+ON s.ArtistId = ar.Id
+GROUP BY ArtistId, ar.ArtistName
+ORDER BY COUNT(ArtistId);
+
+--ordering he artist by amount of songs and displaying the number of songs per artist
+SELECT COUNT(GenreId)
+AS 'Song Count', g.Label
+FROM Song s
+LEFT JOIN Genre g
+ON s.GenreId = g.Id
+GROUP BY GenreId, g.Label
+ORDER BY COUNT(GenreId);
